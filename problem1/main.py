54809efec31d399aee8f7aa93a89e8667eb5eff3
Knock7 <nickochisholm@gmail.com>
2017-05-04 15:46:56 -0600

    Update main.py

--- problem1/main.py
+++ problem1/main.py
@@ -4,3 +4,18 @@ for i in range(0,1000):
   if i%3==0 or i%5==0:
     sum+=i
 print sum
+
+
+//I don't know python so this is JS (answer from wikipedia)
+//I'm trying to see how pushing and merging works on GitHub
+
+function Sum(n, num){
+  tot = 0;
+  for(i = 1; 1<n/num; i++){
+    tot += i*num;
+  }
+  return tot;
+}
+
+ans = Sum(ntot, n1) + Sum(ntot, n2) - Sum(ntot, n1*n2);
+
